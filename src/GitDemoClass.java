abbf84a7aa51bfbcad4a07d0b761aeccf20986cc
pallavismile9 <pallavismile9@gmail.com>
2020-04-13 16:01:55 -0700

    Modified Print message

--- src/GitDemoClass.java
+++ src/GitDemoClass.java
@@ -2,8 +2,8 @@
 public class GitDemoClass {
 
 	public static void main(String[] args) {
-		System.out.println("This is my first Git project");
-
+		System.out.println("This is my first Git project.");
+		System.out.println("This is my second Git project.");
 	}
 
 }
