0d389bdb6ae50a9f07869036a4dab4f2c18f6e31
pallavismile9 <pallavismile9@gmail.com>
2020-04-13 18:39:56 -0700

    in prathamesh branch

--- src/GitDemo2.java
+++ src/GitDemo2.java
@@ -4,7 +4,8 @@ public class GitDemo2 {
 	public static void main(String[] args) {
 		//Testefbffhah
 		//jhdhflkjfhkjas
-		//Switch to pallavi branch
+		//Switch to prathamesh branch
+		//Switch to new branch
 
 	}
 
